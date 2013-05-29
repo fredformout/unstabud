04e0e8fe90c27fe23d86e53a7a293a42deaff0d8
fredformout <fredformout@yandex.ru>
2013-05-29 11:53:05 +0400

    labels features

--- unstabud/CategoriesList/COutcomeCell.h
+++ unstabud/CategoriesList/COutcomeCell.h
@@ -8,12 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import "OutcomeCategory.h"
+#import "TTTAttributedLabel.h"
+
+typedef enum
+{
+    WASTE = 0,
+    LEFT = 1
+}
+valueLabelState;
 
 @interface COutcomeCell : UITableViewCell
+{
+    valueLabelState stateOfValueLabel;
+}
 
 @property (nonatomic, strong) OutcomeCategory *outcomeCategory;
 @property (nonatomic, strong) IBOutlet UILabel *nameLabel;
-@property (nonatomic, strong) IBOutlet UILabel *valueLabel;
+@property (nonatomic, strong) IBOutlet TTTAttributedLabel *valueLabel;
 
 - (void)configureCellWithOutcomeCategory:(OutcomeCategory *)outcomeCategory;
 
--- unstabud/CategoriesList/COutcomeCell.m
+++ unstabud/CategoriesList/COutcomeCell.m
@@ -30,18 +30,38 @@
 
 - (void)revertViewOfValueLabel
 {
-    NSArray *components = [_valueLabel.text componentsSeparatedByString:@"/"];
-    if ([components count] == 2)
+    if (stateOfValueLabel == WASTE)
     {
-        NSString *resultString = [NSString stringWithFormat:@"%.0f р.", [_outcomeCategory.budget floatValue] - [_outcomeCategory.outcome floatValue]];
-        [_valueLabel setText:[CStringUtility spacedMoneyString:resultString]];
-        [_valueLabel setTextColor:[UIColor greenColor]];
+        [_valueLabel setTextColor:[UIColor colorWithRed:46.0/255.0 green:176.0/255.0 blue:0.0/255.0 alpha:1.0]];
+        
+        NSString *resultString = [CStringUtility spacedMoneyString:[NSString stringWithFormat:@"%.0f р.", [_outcomeCategory.budget floatValue] - [_outcomeCategory.outcome floatValue]]];
+        [_valueLabel setText:resultString];
+        
+        stateOfValueLabel = LEFT;
     }
     else
     {
-        [_valueLabel setText:[NSString stringWithFormat:@"%@ р. / %@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.outcome stringValue]], [CStringUtility spacedMoneyString:[_outcomeCategory.budget stringValue]]]];
-        [_valueLabel setTextColor:[UIColor redColor]];
+        [self setupWasteStateOfValueLabel];
+        
+        stateOfValueLabel = WASTE;
     }
+}
+
+- (void)setupWasteStateOfValueLabel
+{
+    NSString *outcomeString = [NSString stringWithFormat:@"%@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.outcome stringValue]]];
+    NSString *budgetString =  [NSString stringWithFormat:@"%@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.budget stringValue]]];
+    
+    [_valueLabel setText:[NSString stringWithFormat:@"%@\n%@", outcomeString, budgetString] afterInheritingLabelAttributesAndConfiguringWithBlock:^ NSMutableAttributedString *(NSMutableAttributedString *mutableAttributedString)
+     {
+         NSRange blackRange = NSMakeRange(0, [outcomeString length]);
+         NSRange grayRange = NSMakeRange([outcomeString length] + 1, [budgetString length]);;
+         
+         [mutableAttributedString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(id)[UIColor blackColor].CGColor range:blackRange];
+         [mutableAttributedString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(id)[UIColor grayColor].CGColor range:grayRange];
+         
+         return mutableAttributedString;
+     }];
 }
 
 - (void)configureCellWithOutcomeCategory:(OutcomeCategory *)outcomeCategory
@@ -49,21 +69,29 @@
     if (outcomeCategory)
     {
         self.outcomeCategory = outcomeCategory;
-        
         [_nameLabel setText:_outcomeCategory.name];
+        [self setupValueLabel];
+    }
+}
+
+- (void)setupValueLabel
+{
+    stateOfValueLabel = WASTE;
+    
+    if ([_outcomeCategory.budgetIsDefined boolValue] == YES)
+    {
+        [self setupWasteStateOfValueLabel];
         
-        if ([_outcomeCategory.budgetIsDefined boolValue] == YES)
-        {
-            [_valueLabel setText:[NSString stringWithFormat:@"%@ р. / %@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.outcome stringValue]], [CStringUtility spacedMoneyString:[_outcomeCategory.budget stringValue]]]];
-            [_valueLabel setUserInteractionEnabled:YES];
-        }
-        else
-        {
-            [_valueLabel setText:[NSString stringWithFormat:@"%@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.outcome stringValue]]]];
-            [_valueLabel setUserInteractionEnabled:NO];
-        }
+        [_valueLabel setUserInteractionEnabled:YES];
+    }
+    else
+    {
+        [_valueLabel setTextColor:[UIColor blackColor]];
         
-        [_valueLabel setTextColor:[UIColor redColor]];
+        NSString *resultString = [NSString stringWithFormat:@"%@ р.", [CStringUtility spacedMoneyString:[_outcomeCategory.outcome stringValue]]];
+        [_valueLabel setText:resultString];
+        
+        [_valueLabel setUserInteractionEnabled:NO];
     }
 }
 
